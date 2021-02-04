c6c80a06f92da76eae139da38cf4e0c13f70dfe7
hq <1422127065@qq.com>
2021-02-04 22:49:18 +0800

    add:用户登录

--- src/main/java/com/hq/SpringbootprogramApplication.java
+++ src/main/java/com/hq/SpringbootprogramApplication.java
@@ -1,9 +1,11 @@
 package com.hq;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.hq.mapper")
 public class SpringbootprogramApplication {
 
     public static void main(String[] args) {
